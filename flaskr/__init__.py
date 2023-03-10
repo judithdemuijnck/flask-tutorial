ac9239c5f06b1e7a6f7803dba8a710b285e07a3c
judithdemuijnck <61882827+judithdemuijnck@users.noreply.github.com>
2023-03-10 12:05:20 +0000

    add database and authentication

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,1 +1,36 @@
-print("hello world")
+import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",  # will be set to a token in production
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
+    )
+
+    if test_config is None:
+        # load the instance config if no other has been passed
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensures the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello World"
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
